f0d2f724c01417282446e7073207af380ea2f848
almanangan <145944481+almanangan@users.noreply.github.com>
2023-10-12 04:47:35 -0700

    Add files via upload

---
+++ labact3-loops.py
@@ -0,0 +1,16 @@
+for i in range(1, 6):
+    for j in range(1, i + 1):
+        print(j, end="")
+    print()
+
+n = 4
+sum = 0
+for i in range(1, n + 1):
+    sum+= i
+print(sum)
+
+for i in range(7,1, -1):
+    for j in range(1, i - 1):
+        print(j, end="")
+    print()
+
